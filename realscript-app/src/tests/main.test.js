e666563e3a979db27fc4fc81f495acdc8573370d
nnrogers515 <nnrogers515@gmail.com>
2021-02-25 03:14:17 -0600

    will have to wait for pipeline

--- realscript-app/src/tests/main.test.js
+++ realscript-app/src/tests/main.test.js
@@ -1,8 +1,9 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import { Route } from 'react-router';
+import Routes from '../routes';
+import Socket from '../modules/socketClient';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+  render(<Routes />);
+  render(<Socket />);
+});
