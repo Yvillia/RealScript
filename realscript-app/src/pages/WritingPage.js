1777cffcc2c251cf6fe9bb9db4a91afd0f1529ca
nnrogers515 <nnrogers515@gmail.com>
2021-04-28 02:16:36 -0500

    added whiteboard feature based on tutorial, look for alternative to cognito

---
+++ realscript-app/src/pages/WritingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "../assets/main.css";
+import "../pages/login.js";
+import Board from "../modules/board";
+import "../assets/board.css";
+
+export default class WritingPage extends Component {
+  render() {
+    return <Board />;
+  }
+}
--- realscript-app/src/routes.js
+++ realscript-app/src/routes.js
@@ -5,6 +5,8 @@ import Editor from "./pages/editor.js";
 import Profile from "./pages/profile.js";
 import Settings from "./pages/settings.js";
 import Login from "./pages/login.js";
+import Whiteboard from "./pages/whiteboard.js";
+import WritingPage from "./pages/WritingPage.js";
 import NotFoundPage from "./pages/404.js";
 
 class Routes extends React.Component {
@@ -17,6 +19,8 @@ class Routes extends React.Component {
           <Route path="/Profile" exact component={Profile} />
           <Route path="/Settings" exact component={Settings} />
           <Route path="/Home" exact component={Home} />
+          <Route path="/Whiteboard" exact component={Whiteboard} />
+          <Route path="/Writing" exact component={WritingPage} />
           <Route path="*" component={NotFoundPage} />
         </Switch>
       </Router>
